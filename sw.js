cfa6ad8cab59cf3349450e2580bc60470ee199c2
lucasstuetzle <lucas.stuetzle@googlemail.com>
2025-02-26 21:02:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,17 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('my-cache').then(cache => {
       return cache.addAll([
-        '/',
+        '/',  // Stelle sicher, dass die root URL mit gecached wird
+        '/index.html',
         '/Übersicht.html',
         '/css/style.css',
         '/js/app.js',
         '/manifest.json',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
+        '/Icons/icon-192x192.png',
+        '/Icons/icon-512x512.png'
       ]);
+    }).catch(error => {
+      console.error('Cache konnte nicht erstellt werden:', error);
     })
   );
 });
@@ -18,7 +21,9 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    }).catch(error => {
+      console.error('Fehler beim Abrufen der Datei:', error);
+      return new Response('Offline, aber Fehler beim Abrufen.');
     })
   );
 });
-
