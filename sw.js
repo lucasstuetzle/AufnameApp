07a51a247710f42105c2648c923343f8c1987da1
lucasstuetzle <lucas.stuetzle@googlemail.com>
2025-03-03 20:31:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,9 +10,9 @@ self.addEventListener('install', event => {
       console.log('Service Worker: Pre-caching der Ressourcen');
       return cache.addAll([
         'https://lucasstuetzle.github.io/AufnameApp/index.html',              // Startseite
-        'https://lucasstuetzle.github.io/AufnameApp/Energieverteilung.html',    
-        'https://lucasstuetzle.github.io/AufnameApp/Grundriss.html',          
-        'https://lucasstuetzle.github.io/AufnameApp/Hausanschluss.html',          
+        'https://lucasstuetzle.github.io/AufnameApp/Unterkategorien/Energieverteilung.html', 
+        'https://lucasstuetzle.github.io/AufnameApp/Unterkategorien/Grundriss.html',          
+        'https://lucasstuetzle.github.io/AufnameApp/Unterkategorien/Hausanschluss.html',          
         'https://lucasstuetzle.github.io/AufnameApp/css/style.css',            // CSS-Datei
         'https://lucasstuetzle.github.io/AufnameApp/js/Berechnung-kWh.js',     // JavaScript-Datei
         'https://lucasstuetzle.github.io/AufnameApp/js/speicher.js',           // JavaScript-Datei
