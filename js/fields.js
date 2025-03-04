fdde829835137505f34a97724f6946b322d68cd2
lucasstuetzle <lucas.stuetzle@googlemail.com>
2025-03-04 08:06:34 +0100

    Create fields.js

---
+++ js/fields.js
@@ -0,0 +1,2 @@
+// Definiere die Felder in einer separaten Datei
+const fields = ["standortName", "standortAdresse", "standortBemerkung", "location", "supply", "capacity", "time", "energy", "author", "circuit"];
