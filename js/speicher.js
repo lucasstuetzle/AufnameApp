532aa6a603057d6be455ac9e3f174967af59bb5f
lucasstuetzle <lucas.stuetzle@googlemail.com>
2025-03-09 18:04:55 +0100

    Update speicher.js

--- js/speicher.js
+++ js/speicher.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Verwende die fields-Konstante aus fields.js
     fields.forEach(field => {
         const fieldElement = document.getElementById(field);
+
         if (fieldElement) {
             console.log(`Feld gefunden: ${field}`);
 
@@ -13,9 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
             // Speichern bei Eingabe
             fieldElement.addEventListener("input", function () {
                 sessionStorage.setItem(field, this.value);
-                console.log(`Gespeichert: ${field} = ${this.value}
-
+                console.log(`Gespeichert: ${field} = ${this.value}`);
             });
+
         } else {
             console.warn(`Feld nicht gefunden: ${field}`);
         }
